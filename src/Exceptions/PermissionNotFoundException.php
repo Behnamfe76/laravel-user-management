62da3d3014172aeaa9509792ea53f2987675cb96
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-04-24 16:27:34 +0330

    Add PermissionNotFoundException for handling permission-related errors

---
+++ src/Exceptions/PermissionNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fereydooni\LaravelUserManagement\Exceptions;
+
+use Exception;
+
+class PermissionNotFoundException extends Exception
+{
+    // Custom exception for permission not found errors
+} 
